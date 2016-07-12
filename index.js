2a30b7c2172257e2f1df17abb418d53f263f2c1f
Glen Keane <glenkeane.94@gmail.com>
2016-07-12 15:24:06 +0100

    basic structure in place, now to generate html reports

---
+++ index.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+
+'use strict'
+
+const minimist = require('minimist')
+const fs = require('fs')
+const path = require('path')
+const stream = require('stream')
+const assert = require('assert')
+const JSONStream = require('JSONStream')
+const help = fs.readFileSync(path.join(__dirname, 'help.txt'), 'utf8')
+const handleBars =
+
+function start () {
+  const argv = minimist(process.argv.slice(2), {
+    boolean: ['help'],
+    alias: {
+      input: 'i',
+      version: 'v',
+      help: 'h'
+    },
+    default: {
+    }
+  })
+
+  argv.outputPath = path.join(process.cwd(), 'report.html')
+  argv.outputPathFolder = path.dirname(argv.outputPath)
+
+  if (argv.version) {
+    console.log('autocannon', 'v' + require('./package').version)
+    console.log('node', process.version)
+    return
+  }
+
+  if (argv.help) {
+    console.error(help)
+    return
+  }
+
+  if (process.stdin.isTTY) {
+    if (!argv.input) {
+      console.error('input (-i) is required when not piping into this')
+      console.error(help)
+      return
+    }
+
+    argv.inputPath = path.isAbsolute(argv.input) ? argv.input : path.join(process.cwd(), argv.input)
+    const results = require(argv.inputPath)
+    generateReport(results)
+  } else {
+    const stream = JSONStream.parse()
+    stream.on('data', generateReport)
+    process.stdin.pipe(stream)
+  }
+
+}
+
+function generateReport (results) {
+  console.log(results)
+}
+
+if (require.main === module) {
+  start()
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Autocannon-reporter
+
+A html reporter for autocannon
+
+# License
+
+[MIT](./LICENSE). Copyright (c) 2016 Glen Keane and other contributors.
