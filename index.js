955fbe2acb853682ae775c7979f15149783ac8ff
Glen Keane <glenkeane.94@gmail.com>
2016-07-12 16:03:16 +0100

    handlebars to be removed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const minimist = require('minimist')
 const fs = require('fs')
 const path = require('path')
 const JSONStream = require('JSONStream')
+const Handlebars = require('handlebars')
 const help = fs.readFileSync(path.join(__dirname, 'help.txt'), 'utf8')
 
 function start () {
@@ -43,20 +44,33 @@ function start () {
 
     argv.inputPath = path.isAbsolute(argv.input) ? argv.input : path.join(process.cwd(), argv.input)
     const results = require(argv.inputPath)
-    generateReport(results)
+    generateReport(results, (report) => { writeReport(report, argv.outputPath ()=>{}) })
   } else {
     const stream = JSONStream.parse()
-    stream.on('data', generateReport)
+    stream.on('data', (results) => {
+      generateReport(results, argv.outputPath)
+    })
     process.stdin.pipe(stream)
   }
 }
 
 function generateReport (results, cb) {
   console.log(results)
+  const filename = path.join(__dirname, 'template', 'index.html')
+  const template = fs.readFileSync(filename, 'utf8')
+  const partialsPath = path.join(__dirname, 'template', 'partials')
+  const partials = fs.readdirSync(partialsPath)
+  partials.forEach((partial) => {
+      Handlebars.registerPartial(Path.basename(partial, '.html'), fs.readFileSync(Path.join(partialsPath, partial), 'utf8'))
+  })
+
+  const view = Handlebars.compile(template);
+  const report = view(results)
+  cb(report)
 }
 
-function writeReport (report, path) {
-
+function writeReport (report, path, cb) {
+  fs.writeFile(path, report, cb)
 }
 
 module.exports.generateReport = generateReport
