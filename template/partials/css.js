09a6deafd3e1274a442b7b0f25a902b6a15f9713
Jane Quinn <jane.quinn@nearform.com>
2016-07-15 10:22:20 +0100

    started work on html page

--- template/partials/css.js
+++ template/partials/css.js
@@ -6,7 +6,40 @@ module.exports = function (results, hx) {
   <style>
     body {
       font-family: 'Roboto', sans-serif;
+      background: #c8edff;
     }
+    .report {
+      display: -webkit-box;
+      display: -moz-box;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: stretch;
+      align-content: space-around
+
+    }
+    .object {
+      background: #66cafc;
+      margin: 0 0 3% 0;
+      padding: 0% 10% 1% 5%;
+    }
+    .table {
+      display: flex;
+      flex-direction: column;
+      background: #2C3E50;
+      color: #ddd;
+
+    }
+
   </style>
   `
 }
+/* body {
+  font-family: 'Roboto', sans-serif;
+}
+.heading{
+  margin: 20%
+}
+-webkit-flex-flow: row wrap;*/
--- template/partials/report.js
+++ template/partials/report.js
@@ -4,10 +4,29 @@ const prettyBytes = require('pretty-bytes')
 module.exports = function (results, hx) {
   return hx`
   <div class='report'>
-    ${results.title ? hx`<h1>Results for ${results.title}</h1>` : hx`<h1>Autocannon results</h1>`}
-    <div class='latency'>
+    <div class='heading'>
+      ${results.title ? hx`<h1>Results for ${results.title}</h1>` : hx`<h1>Autocannon results</h1>`}
+    </div>
+    <div class='meta object'>
+      <h2>General info</h2>
+      <ul class='table text'>
+        <li><b>Errors:</b> ${results.errors}</li>
+        <li><b>Timeouts:</b> ${results.timeouts}</li>
+        <li><b>Runtime:</b> ${results.duration}</li>
+        <li><b>Start time:</b> ${new Date(results.start)}</li>
+        <li><b>Finish time:</b> ${new Date(results.finish)}</li>
+        <li><b>Connections:</b> ${results.connections}</li>
+        <li><b>Pipelining:</b> ${results.pipelining}</li>
+        <li><b>1xx responses:</b> ${results['1xx']}</li>
+        <li><b>2xx responses:</b> ${results['2xx']}</li>
+        <li><b>3xx responses:</b> ${results['3xx']}</li>
+        <li><b>4xx responses:</b> ${results['4xx']}</li>
+        <li><b>5xx responses:</b> ${results['5xx']}</li>
+      </ul>
+    </div>
+    <div class='latency object'>
       <h2>Latency</h2>
-      <table style="width:100%">
+      <table class='table' style="width:100%">
         <tr>
           <th>Stat</th>
           <th>Value</th>
@@ -22,9 +41,9 @@ module.exports = function (results, hx) {
         }
       </table>
     </div>
-    <div class='throughput'>
+    <div class='throughput object'>
       <h2>Throughput</h2>
-      <table style="width:100%">
+      <table class='table' style="width:100%">
         <tr>
           <th>Stat</th>
           <th>Value</th>
@@ -38,23 +57,6 @@ module.exports = function (results, hx) {
           })
         }
       </table>
-      <div class='meta'>
-        <h2>General info</h2>
-        <ul>
-          <li><b>Errors:</b> ${results.errors}</li>
-          <li><b>Timeouts:</b> ${results.timeouts}</li>
-          <li><b>Runtime:</b> ${results.duration}</li>
-          <li><b>Start time:</b> ${new Date(results.start)}</li>
-          <li><b>Finish time:</b> ${new Date(results.finish)}</li>
-          <li><b>Connections:</b> ${results.connections}</li>
-          <li><b>Pipelining:</b> ${results.pipelining}</li>
-          <li><b>1xx responses:</b> ${results['1xx']}</li>
-          <li><b>2xx responses:</b> ${results['2xx']}</li>
-          <li><b>3xx responses:</b> ${results['3xx']}</li>
-          <li><b>4xx responses:</b> ${results['4xx']}</li>
-          <li><b>5xx responses:</b> ${results['5xx']}</li>
-        </ul>
-      </div>
     </div>
   </div>
   `
