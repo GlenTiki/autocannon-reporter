b4a06116f2945d16d4bec509d47e2e07b2a3e8fd
Jane Quinn <jane.quinn@nearform.com>
2016-07-15 16:42:45 +0100

    updated test

--- test/report.test.js
+++ test/report.test.js
@@ -8,10 +8,25 @@ const autocannonReporter = require('..')
 const sampleResult = require('./sampleResult.json')
 const report = autocannonReporter.buildReport(sampleResult)
 const expect = Code.expect
+function minTwoDigits (n) {
+  return (n < 10 ? '0' : '') + n
+}
+
+function datestuff (date) {
+  date = new Date(date)
+  /* date.getMonth() + ' ' + date.getDate() + ', ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+*/
+  var temp1 = date.toDateString().substring(4)
+  var temp2 = temp1.substring(0, temp1.length - 4)
+  temp2 = temp2.trim()
+  var temp3 = temp2 + ', ' + minTwoDigits(date.getHours()) + ':' + minTwoDigits(date.getMinutes()) + ':' + minTwoDigits(date.getSeconds())
+  var ret = temp3
+  return ret
+}
 
 lab.test('Report output should contain a start and finish time', (done) => {
-  expect(report).to.contain(new Date(sampleResult.start).toString())
-  expect(report).to.contain(new Date(sampleResult.finish).toString())
+  expect(report).to.contain(datestuff(new Date(sampleResult.start).toString()))
+  expect(report).to.contain(datestuff(new Date(sampleResult.finish).toString()))
   done()
 })
 
