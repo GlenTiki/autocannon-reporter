e613d16811efd881bc1f2a0277730ee0c32886e8
Jane Quinn <jane.quinn@nearform.com>
2016-07-19 15:31:00 +0100

    pie chart finished

--- template/partials/report.js
+++ template/partials/report.js
@@ -1,4 +1,6 @@
 'use strict'
+const h = require('hyperscript')
+const hyperx = require('hyperx')
 const prettyBytes = require('pretty-bytes')
 const moment = require('moment')
 function datestuff (date) {
@@ -6,7 +8,8 @@ function datestuff (date) {
 }
 
 
-module.exports = function (results, hx) {
+module.exports = function (results) {
+  const hx = hyperx(h)
   return hx`
   <div>
     <div class='title'>
@@ -31,53 +34,98 @@ module.exports = function (results, hx) {
           <li><b>Latency average:</b> ${results.latency.average} ms</li>
         </ul>
       </div>
-      <div class='object latency'>
-        <div class='heading' onclick="growDiv(this)">
-        <h2 class='symbol'>-</h2>
-          <h2>Latency</h2>
-        </div>
-        <div class='content'>
-          <div class='measuringWrapper'>
-            <table class='table' style="width:100%">
-              <tr>
-                <th>Stat</th>
-                <th>Value</th>
-              </tr>
-              ${
-                Object.keys(results.latency).map((key) => {
-                  return hx`<tr>
-                    <td>${key}</td>
-                    <td>${results.latency[key]}</td>
-                  </tr>`
-                })
-              }
-            </table>
-          </div>
-        </div>
+      ${results['2xx'] + results.non2xx > 0 ? pie(results, hx) : ''}
+      ${results['2xx'] + results.non2xx > 0 ? panel1(results, hx) : panelWarn(results, hx)}
+      ${results['2xx'] + results.non2xx > 0 ? panel2(results, hx) : ''}
+    </div>
+  </div>
+  `
+}
+
+function panel1 (results, hx) {
+  return hx`
+  <div class='object latency'>
+    <div class='heading' onclick="growDiv(this)">
+    <h2 class='symbol'>-</h2>
+      <h2>Latency</h2>
+    </div>
+    <div class='content'>
+      <div class='measuringWrapper'>
+        <table class='table' style="width:100%">
+          <tr>
+            <th>Stat</th>
+            <th>Value</th>
+          </tr>
+          ${
+            Object.keys(results.latency).map((key) => {
+              return hx`<tr>
+                <td>${key}</td>
+                <td>${results.latency[key]}</td>
+              </tr>`
+            })
+          }
+        </table>
       </div>
-      <div class='object throughput'>
-        <div class='heading' onclick="growDiv(this)">
-          <h2 class='symbol'>-</h2>
-          <h2>Throughput</h2>
-        </div>
-        <div class='content'>
-          <div class='measuringWrapper'>
-            <table class='table' style="width:100%">
-              <tr>
-                <th>Stat</th>
-                <th>Value</th>
-              </tr>
-              ${
-                Object.keys(results.throughput).map((key) => {
-                  return hx`<tr>
-                    <td>${key}</td>
-                    <td>${prettyBytes(results.throughput[key])}</td>
-                  </tr>`
-                })
-              }
-            </table>
-          </div>
-        </div>
+    </div>
+  </div>
+  `
+}
+
+function panel2 (results, hx) {
+  return hx`
+  <div class='object throughput'>
+    <div class='heading' onclick="growDiv(this)">
+      <h2 class='symbol'>-</h2>
+      <h2>Throughput</h2>
+    </div>
+    <div class='content'>
+      <div class='measuringWrapper'>
+        <table class='table' style="width:100%">
+          <tr>
+            <th>Stat</th>
+            <th>Value</th>
+          </tr>
+          ${
+            Object.keys(results.throughput).map((key) => {
+              return hx`<tr>
+                <td>${key}</td>
+                <td>${prettyBytes(results.throughput[key])}</td>
+              </tr>`
+            })
+          }
+        </table>
+      </div>
+    </div>
+  </div>
+  `
+}
+
+function panelWarn (results, hx) {
+  return hx`
+  <div class='object throughput'>
+    <div class='heading' onclick="growDiv(this)">
+      <h2 class='symbol'>-</h2>
+        <h2>Warning</h2>
+    </div>
+    <div class='content'>
+      <div class='measuringWrapper'>
+        <h3 color='red'>No graphs to display because of failed autocannon run (0 responses received)</h3>
+      </div>
+    </div>
+  </div>
+  `
+}
+
+function pie (results, hx) {
+  return hx `
+  <div class='object throughput'>
+    <div class='heading' onclick="growDiv(this)">
+      <h2 class='symbol'>-</h2>
+        <h2>Response Types Piechart</h2>
+    </div>
+    <div class='content graph'>
+      <div class='measuringWrapper'>
+        <div class="ct-chart ct-perfect-fourth"></div>
       </div>
     </div>
   </div>
--- template/partials/scripts.js
+++ template/partials/scripts.js
@@ -1,26 +1,65 @@
 'use strict'
 
-module.exports = function (results, hx) {
-  // put any clientside interactivity scripts between script tags
-  // if minified libs needed, read them sync and put the buffer.toString
-  // in there
-  return hx`
-  <script>
-  function growDiv(e) {
-    var header = e
-    var objectContainer = header.parentElement
-    var symbol = objectContainer.getElementsByClassName("symbol")[0]
-    var content = objectContainer.getElementsByClassName("content")[0]
-    if (content.clientHeight !== 20) {
-      content.style.height = 0;
-      symbol.innerText = '+'
-    } else {
-      var wrapper = content.getElementsByClassName("measuringWrapper")[0]
-      content.style.height = wrapper.clientHeight + "px";
-      symbol.innerText = '-'
-    }
 
-  }
-  </script>
+module.exports = function (results) {
+  return `
+    var results = ${JSON.stringify(results)}
+    ${growDiv.toString()}
+    ${main.toString()}
+    main(Chartist, results)
+
   `
+}
+
+function main (chartist, results) {
+  var labels = ['1xx', '2xx', '3xx', '4xx', '5xx', 'non2xx', 'timeouts']
+  var nonZeros = []
+  var seriesValues = []
+  var options = {
+    fullWidth: true,
+    height: 450
+  }
+  var responsiveOptions = [
+    ['screen and (min-width: 640px)', {
+      chartPadding: 30,
+      labelOffset: 110,
+      labelDirection: 'explode',
+      labelInterpolationFnc: function (value) {
+        return value
+      }
+    }],
+    ['screen and (min-width: 1024px)', {
+      labelOffset: 90,
+      chartPadding: 20
+    }]
+  ]
+  labels.forEach(function (label) {
+    if (results[label] !== 0) {
+      console.log(label)
+      nonZeros.push(label)
+    }
+  })
+  nonZeros.forEach(function (value) {
+    seriesValues.push(results[value])
+    console.log(seriesValues)
+  })
+  chartist.Pie('.ct-chart', {
+    labels: nonZeros,
+    series: seriesValues
+  }, options, responsiveOptions)
+}
+
+function growDiv (e) {
+  var header = e
+  var objectContainer = header.parentElement
+  var symbol = objectContainer.getElementsByClassName('symbol')[0]
+  var content = objectContainer.getElementsByClassName('content')[0]
+  if (content.clientHeight !== 20) {
+    content.style.height = 0
+    symbol.innerText = '+'
+  } else {
+    var wrapper = content.getElementsByClassName('measuringWrapper')[0]
+    content.style.height = wrapper.clientHeight + 'px'
+    symbol.innerText = '-'
+  }
 }
